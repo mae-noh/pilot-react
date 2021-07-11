ae8009f5feb11e09bcb4820c3eb165787aa5d6e8
mae <myeongheeroh@gmail.com>
2021-07-12 01:47:59 +0900

    Login success

--- results/mae-noh/src/views/Login.vue
+++ results/mae-noh/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <div class="d-flex justify-content-center">
-  <b-form @submit.stop.prevent="onSubmit" @reset="onReset" v-if="show">
+  <b-form @submit.stop.prevent="Login(account, password)" @reset="onReset" v-if="show">
         <b-form-group
             class="mb-2"
             id="input-group-account"
@@ -10,7 +10,7 @@
         >
           <b-form-input
               id="account"
-              v-model="form.account"
+              v-model="account"
               type="account"
               placeholder="Enter Account"
               required
@@ -19,13 +19,13 @@
         <b-form-group class="mb-3" id="input-group-password" label="password" label-for="password">
           <b-form-input
               id="password"
-              v-model="form.password"
+              v-model="password"
               placeholder="Enter Password"
               required
           ></b-form-input>
         </b-form-group>
         <div class="d-flex justify-content-center">
-          <b-button class="me-1" type="submit" variant="primary">Submit</b-button>
+          <b-button class="me-1" type="submit" variant="primary">Login</b-button>
           <b-button type="reset" variant="danger">Reset</b-button>
         </div>
   </b-form>
@@ -36,27 +36,39 @@
 <script lang="ts">
 
 import {Component, Vue} from 'vue-property-decorator'
+import axios from "axios"
 
 @Component
 export default class Login extends Vue{
 
-  form: { account: string, password: string } = {account: 'devbadak', password: '1234'};
+  account  = 'devbadak';
+  password  = '1234';
   show = true;
 
-  onSubmit = (event : any) => {
-    event.preventDefault()
-    console.log(JSON.stringify(this.form))
+  Login = (account : string, password : string) => {
+    console.log("account: " + account + " password: " + password);
+    // POST
+    axios.post('http://localhost:5000/auth/login', JSON.stringify({"account": account, "password": password}),
+        { headers: { 'Content-Type': 'application/json' } })
+        .then(function(response) {
+          if (response.status == 200) {
+            console.log(response);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
   };
 
   onReset(event : any): void {
     event.preventDefault()
     // Reset our form values
-    this.form.account = ''
-    this.form.password = ''
+    this.account = '';
+    this.password = '';
     // Trick to reset/clear native browser form validation state
-    this.show = false
+    this.show = false;
     this.$nextTick(() => {
-      this.show = true
+      this.show = true;
     })
   };
 
